266d57f51a8db66a1b85c9b9ad59ec7b4213636d
abluenautilus <abluenautilus@gmail.com>
2024-11-01 12:25:18 -0700

    small fixes

--- USBFatFSTest.cpp
+++ USBFatFSTest.cpp
@@ -5,7 +5,6 @@
 using namespace daisy;
 using namespace patch_sm;
 
-
 DaisyPatchSM_USB   hw;
 FIL            file; /**< Can't be made on the stack (DTCMRAM) */
 FatFSInterface fsi;
@@ -14,7 +13,6 @@ FRESULT fileResult;
 #define IO_BUFFER_SIZE 100
 #define TEST_FILE_NAME "USB_Test.txt"
 char testFilePath[IO_BUFFER_SIZE];
-
 bool isUsbConnected = false;
 bool isUsbConfigured = false;
 bool wasConfigLoadAttempted = false;
@@ -32,22 +30,19 @@ void AudioCallback(AudioHandle::InputBuffer  in,
     }
 }
 
-
+/* USB Host callbacks */
 void USBConnectCallback(void* userdata) {
 	isUsbConnected = true;
     hw.PrintLine("USB Connected");
 }
-
 void USBDisconnectCallback(void* userdata) {
 	isUsbConnected = false;
 	isUsbConfigured = false;
     hw.PrintLine("USB Disconnected");
 }
-
 void USBClassActiveCallback(void* userdata) {
 	isUsbConfigured = true;
 }
-
 void USBErrorCallback(void* userdata) {
     hw.PrintLine("USB Error");
 }
@@ -61,10 +56,9 @@ int main(void)
     /* SET UP USB*/
     const char* usbPath = hw.fatfs_interface.GetUSBPath();
     snprintf(testFilePath, IO_BUFFER_SIZE, "%s%s", usbPath, TEST_FILE_NAME);
-
     hw.PrepareMedia(USBConnectCallback, USBDisconnectCallback, USBClassActiveCallback, USBErrorCallback);
 
-    /** Write/Read text file */
+    /** Strings for write/read to text file */
     const char *test_string = "Testing Daisy Patch SM";
     const char *test_fname  = "DaisyPatchSM-USBTest.txt";
 
@@ -79,7 +73,7 @@ int main(void)
 
         if ((now%5000)==0) {
 
-            FRESULT fres = FR_DENIED; /**< Unlikely to actually experience this */
+            FRESULT fres = FR_DENIED; 
          
             /** Write Test */
             fres = f_open(&file, testFilePath, (FA_CREATE_ALWAYS | FA_WRITE));
@@ -89,18 +83,21 @@ int main(void)
                 size_t len = strlen(test_string);
                 fres       = f_write(&file, test_string, len, &bw);
             } else {
-                hw.PrintLine("F_open failed with code: %d",fres);
+                hw.PrintLine("Write test f_open failed with code: %d",fres);
             }
             f_close(&file);
             if(fres == FR_OK)
             {
                 /** Read Test only if Write passed */
-                if(f_open(&file, test_fname, (FA_OPEN_EXISTING | FA_READ)) == FR_OK)
+                fres = f_open(&file, test_fname, (FA_OPEN_EXISTING | FA_READ);
+                if(fres == FR_OK)
                 {
                     UINT   br = 0;
                     char   readbuff[32];
                     size_t len = strlen(test_string);
                     fres       = f_read(&file, readbuff, len, &br);
+                } else {
+                    hw.PrintLine("Read test f_open failed with code: %d",fres);
                 }
                 f_close(&file);
             }
